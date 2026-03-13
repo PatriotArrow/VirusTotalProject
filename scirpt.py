04c79dab60a22c71055c824e5f29f50ca1e0c05b
PatriotArrow <40357308+PatriotArrow@users.noreply.github.com>
2026-03-13 10:58:30 +0000

    main logic

--- scirpt.py
+++ scirpt.py
@@ -3,6 +3,35 @@ import requests
 import json
 from dotenv import load_dotenv
 
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+hashes = open("hash.txt").read().splitlines()
+# print(hashes)
+
+#session init
+session = requests.Session()
+session.headers = {'X-Apikey': API_KEY}
+
+
+# url = "https://www.virustotal.com/api/v3/monitor_partner/hashes/05dc7b24be0359720c2ac68c53966378063e51d23251cb9993be37300b195265/items"
+# url = f"https://www.virustotal.com/api/v3/files/{hash}"
+
+
+for hash in hashes:
+
+    url = "https://www.virustotal.com/api/v3/files/{hash}"
+    response = session.get(url)
+    if response.status_code == 200:
+        print(response.json())
+        with open(f'json_files/{hash}.json', 'w', encoding='utf-8') as f:
+            json.dump(response.json(), f, indent=4)
+
+
+    
+
 
 
 #https://realpython.com/python-requests/#get-started-with-pythons-requests-library
+# https://docs.virustotal.com/reference/monitorpartner-hashes-items
+# https://stackoverflow.com/questions/12309269/how-do-i-write-json-data-to-a-file
